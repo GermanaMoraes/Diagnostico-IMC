b3b7a98ba779d5a16fc4b1b3f54ef81707eceaf4
GermanaMoraes <germanamoraes95@gmail.com>
2022-06-14 19:18:22 -0300

    Ficha Diagnostica\Program.cs

---
+++ Ficha Diagnostica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ficha_Diagnostica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
