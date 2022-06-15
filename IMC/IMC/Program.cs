73eead642913657068107119e7575bf59805257e
GermanaMoraes <germanamoraes95@gmail.com>
2022-06-15 07:57:10 -0300

    Update Program.cs

--- IMC/IMC/Program.cs
+++ IMC/IMC/Program.cs
@@ -6,20 +6,29 @@ namespace IMC
     {
         static void Main(string[] args)
         {
-
-            
+                    
 
             Console.WriteLine("Vamos calcular sua ficha diagnóstica! A seguir digite alguns dados seus para mim.");
             Console.WriteLine("Digite seu nome");
             string nome = Console.ReadLine();
+            Console.WriteLine("Digite F para Feminino ou M para Masculino");
+            string sexo= Console.ReadLine();
             Console.WriteLine("Digite sua idade");
             int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua altura em metros");
+            double altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu peso");
+            double peso= double.Parse(Console.ReadLine());
 
 
             //Imprimindo o Cabeçalho
             Console.WriteLine("Diagnóstico Prévio");
-            Console.Write();
-
+            Console.WriteLine($"Nome:{nome}");
+            Console.WriteLine("Sexo:");
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine("Altura:");
+            Console.WriteLine("Peso");
+            
             //Calculando a categoria
             if (idade < 12)
             { Console.WriteLine("Categoria: Infantil"); }
@@ -30,6 +39,7 @@ namespace IMC
             else
             { Console.WriteLine("Categoria: Idoso");    }
 
+            //Calculando o IMC
 
                           
         }
