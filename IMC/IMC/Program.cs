a02816c098f32c4cc9fbd6320a087b4ea01cc1ef
GermanaMoraes <germanamoraes95@gmail.com>
2022-06-15 09:28:39 -0300

    Update Program.cs

--- IMC/IMC/Program.cs
+++ IMC/IMC/Program.cs
@@ -11,8 +11,8 @@ namespace IMC
             Console.WriteLine("Vamos calcular sua ficha diagnóstica! A seguir digite alguns dados seus para mim.");
             Console.WriteLine("Digite seu nome");
             string nome = Console.ReadLine();
-            Console.WriteLine("Digite F para Feminino ou M para Masculino");
-            string sexo= Console.ReadLine();
+            Console.WriteLine("Digite 1 para Feminino ou 2 para Masculino");
+            int sexo= int.Parse(Console.ReadLine());
             Console.WriteLine("Digite sua idade");
             int idade = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite sua altura em metros");
@@ -20,13 +20,16 @@ namespace IMC
             Console.WriteLine("Digite seu peso");
             double peso= double.Parse(Console.ReadLine());
 
-
             //Imprimindo o Cabeçalho
             Console.WriteLine("Diagnóstico Prévio");
             Console.WriteLine($"Nome:{nome}");
 
-            //Imprimindo o sexo(olhar como fazer)
-            
+            //Imprimindo o sexo
+            if(sexo==1)
+            { Console.WriteLine("Sexo: Feminino."); }
+            if (sexo==2)
+            { Console.WriteLine("Sexo: Masculino"); }
+                        
             Console.WriteLine($"Idade: {idade}");
             Console.WriteLine($"Altura:{altura}"); 
             Console.WriteLine($"Peso: {peso}");
@@ -51,14 +54,37 @@ namespace IMC
             //riscos
             if(imc<20)
             {
-                Console.WriteLine("Muitas complicações de saúde como doenças pulmonares" +
+                Console.WriteLine("Riscos: Muitas complicações de saúde como doenças pulmonares" +
                 " e cardiovasculares podem estar associadas ao baixo peso");
             }
+            if(imc>=20 && imc<=24)
+            { Console.WriteLine("Riscos: Seu peso está ideal para suas referências"); }
+            if(imc>=25 && imc<=29)
+            { Console.WriteLine("Riscos: Aumento de peso apresenta risco moderado para outras doença crônicas e cardiovasculares"); }
+            if (imc>=30 && imc<=35)
+            { Console.WriteLine("Riscos: Quem tem obesidade vai estar mais exposto a doenças graves e ao risco de mortalidade"); }
+            if(imc>=36)
+            { Console.WriteLine("Riscos: O obeso mórbito vive menos, tem alto risco de mortalidade geral por diversas causas"); }
+
 
             //recomendações
-                          
+
+            if (imc < 20)
+            {
+                Console.WriteLine("Recomendação inicial: Inclua carboidratos simples em sua dieta, além de proteínas " +
+                    "indispensáveis para ganho de massa magra. Procure um profissional.");
+            }
+            if (imc >= 20 && imc <= 24)
+            { Console.WriteLine("Recomendação inicial: Mantenha uma dieta saudável e faça seus exames periódicos."); }
+            if (imc >= 25 && imc <= 29)
+            { Console.WriteLine("Recomendação inicial:Adote um tratamento baseado em dieta balanceada, exercício fisíco e medicação." +
+                "A ajuda de um profissional pode ser interessante."); }
+            if (imc >= 30 && imc <= 35)
+            { Console.WriteLine("Recomendação inicial: Adote uma dieta alimentar rigorosa, com acompanhamento" +
+                "de um nutricionista e um médico especialista(encocríno)"); }
+            if(imc>=36)
+            { Console.WriteLine("Recomendação inicial: Procure com urgência o acompanhamento de um nutricionista para realizar reeducação alimentar, um psicólogo e um médico especialista (endocríno)."); }
         }
-        
-        
+                
     }
 }
